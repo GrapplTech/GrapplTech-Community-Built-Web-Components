54ca621dd56d201d78903275e93bee63f86cb14c
Bharathkumar1522 <bk152002@gmail.com>
2024-06-28 12:37:56 +0530

    Added Hero Section component template by Bharathkumar Sagi

---
+++ BharathKumarSagiHeroSection/heroSection/heroSection.js
@@ -0,0 +1,46 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+nextDom.onclick = function(){
+    showSlider('next');
+}
+prevDom.onclick = function(){
+    showSlider('prev');
+}
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+let runTimeOut;
+let runAutoRun  = setTimeout( ()=>{
+    nextDom.click();
+},timeAutoNext);
+
+function showSlider(type){
+    let itemSlider = document.querySelectorAll('.carousel .list .item');
+    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+
+    if(type === 'next'){
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carouselDom.classList.add('next');
+    }else{
+        let positionLastItem = itemSlider.length -1;
+        listItemDom.prepend(itemSlider[positionLastItem]);
+        thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+        carouselDom.classList.add('prev');
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() =>{
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev')
+    },timeRunning)
+
+    clearTimeout(runAutoRun);
+    runAutoRun = setTimeout( ()=>{
+        nextDom.click();
+    },timeAutoNext);
+    
+}
