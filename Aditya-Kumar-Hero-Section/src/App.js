eacde84059d12e0743cb174c62de38758a06bff8
Aditya Kumar <adityapal1409@gmail.com>
2024-06-30 13:24:11 +0530

    create Hero component

--- Aditya-Kumar-Hero-Section/src/App.js
+++ Aditya-Kumar-Hero-Section/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<Hero />
 		</div>
 	);
 }
