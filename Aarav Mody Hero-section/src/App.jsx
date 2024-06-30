3d28e00d23f01ce0edd5be8acafad89912436774
aaravmody <aaravmody2@gmail.com>
2024-06-30 22:02:08 +0530

    Added responsiveness and footer component

--- Aarav Mody Hero-section/src/App.jsx
+++ Aarav Mody Hero-section/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import Footer from './Components/Footer'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Navbar />
     <Hero />
+    <Footer />
     </>
   )
 }
